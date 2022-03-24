758e79159c319792a6d1e105f4a0a885ef069f1f
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-03-24 14:13:08 +0100

    vue structure done

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,53 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="post-container">
+                <div v-for="elemento in posts" 
+                :key="elemento.id"  
+                class="post-card">
+                    <p>{{elemento.title}}</p>
+                    <p>
+                        {{elemento.content}}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data(){
+        return{
+            posts:[],
+        }
+    },
+    created(){
+        this.getPosts();
+    },
+    methods:{
+        getPosts: function() {
+            axios
+            .get("/api/posts")
+            .then((response)=>{
+                this.posts = response.data;
+            })
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.post-container {
+    margin-top: 100px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    .post-card {
+        border: 1px solid black;
+        width: 30%;
+        margin: 10px auto;
+    }
+}
+</style>
